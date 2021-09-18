15760b2167f06aa8b0882c0a88b9ac406a2405b6
Alabama Whiskey <amwtablet@gmail.com>
2021-09-18 16:57:13 -0400

    html framework complete for now, question array added to js

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+//Array for all questions and answers
+let questions = [{
+    prompt: "Commonly used data types do NOT include:",
+    options: ["Strings", "Booleans", "Alerts", "Numbers"],
+    answer: "Alerts"
+},
+{
+    prompt: "The condition in an if/else statement is enclosed with _______.",
+    options: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+    answer: "Parentheses"
+},
+{
+    prompt: "Arrays in JavaScript can be used to store ________.",
+    options: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+    answer: "substr( )"
+},
+{
+    prompt: "String Values must be enclosed within ______ when being assigned to variables.",
+    options: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+    answer: "Quotes"
+},
+{
+    prompt: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["JavaScript", "Terminal/Bash", "for Loops", "console.log()"],
+    answer: "console.log()"
+}]
+
+//Defining initial objects
